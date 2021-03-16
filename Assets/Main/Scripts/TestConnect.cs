7dcae5fded24ed9f9c85023e8e76faa27293f61a
SKY <akash.rm.roy@gmail.com>
2021-03-16 23:25:26 +0530

    Project Initial Commit

---
+++ Assets/Main/Scripts/TestConnect.cs
@@ -0,0 +1,29 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    public string gameVersion = "0.0.1";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Connecting to server!");
+        PhotonNetwork.GameVersion = gameVersion; 
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to server!");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        print("Disconnected from server for reason: " + cause.ToString());
+    }
+}
