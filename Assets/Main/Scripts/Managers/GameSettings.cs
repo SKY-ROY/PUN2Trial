3e322373744045ec23f8648bbc95eb8136137c4c
SKY <akash.rm.roy@gmail.com>
2021-03-29 23:09:42 +0530

    SingletonScriptableObject.cs, MasterManager.cs, and GameSettings.cs added

---
+++ Assets/Main/Scripts/Managers/GameSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(menuName = "Manager/GameSettings")]
+
+public class GameSettings : ScriptableObject
+{
+    [SerializeField]
+    private string _gameVersion = "0.0.0";
+    public string GameVersion { get { return _gameVersion; } }
+
+    [SerializeField]
+    private string _nickName = "askyr";
+    public string NickName 
+    { 
+        get 
+        {
+            int val = Random.Range(0, 9999);
+            return _nickName + val.ToString(); 
+        } 
+    }
+}
---
+++ Assets/Main/Scripts/Managers/MasterManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(menuName = "Singletons/MasterManager")]
+
+public class MasterManager : SingletonScriptableObject<MasterManager>
+{
+    [SerializeField]
+    private GameSettings _gameSettings;
+    public static GameSettings GameSettings { get { return Instance._gameSettings; } }
+}
