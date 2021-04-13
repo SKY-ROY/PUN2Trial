9d26a6e9563218d538d4f826d20cfa6052bcef55
SKY <akash.rm.roy@gmail.com>
2021-04-13 13:45:33 +0530

    Room Listing completed

---
+++ Assets/Main/Scripts/UI/Rooms/CreateRoomMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private TMP_Text _roomName;
+
+    public Button createRoom_button;
+
+    private void Start()
+    {
+        // binding listener to createRoom button
+        createRoom_button.onClick.AddListener(onClick_CreateRoom);
+    }
+
+    public void onClick_CreateRoom()
+    {
+        // connection check
+        if (!PhotonNetwork.IsConnected)
+            return;
+
+        /* Create or join Room */
+
+        // defining options for the room 
+        RoomOptions options = new RoomOptions();
+        
+        // player limit
+        options.MaxPlayers = 4;
+
+        string name;
+        if(_roomName.text.Equals(null))
+        {
+            name = "basic";
+        }
+        else
+        {
+            name = _roomName.text;
+        }
+
+        PhotonNetwork.JoinOrCreateRoom(name, options, TypedLobby.Default);    
+    }
+
+    // callback method for successful room creation
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room successfully");
+    }
+
+    // callback method for failed room creation
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.LogError("Room creation failed: " + message);
+    }
+}
---
+++ Assets/Main/Scripts/UI/Rooms/RoomListingMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomListingMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Transform _content;
+
+    [SerializeField]
+    private RoomListing _roomListing;
+
+    private List<RoomListing> _listings = new List<RoomListing>();
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (RoomInfo info in roomList)
+        {
+            if(info.RemovedFromList)
+            {
+                // Removed from Rooms List
+
+                // locate the index for the room in the list through name which has been deleted
+                int index = _listings.FindIndex(x => x.RoomInfo.Name == info.Name);
+
+                // if we find the room item being removed in the local list then destroy its gameObject in the UI
+                if(index != -1)
+                {
+                    Destroy(_listings[index].gameObject);
+                    _listings.RemoveAt(index);
+                }
+            }
+            else
+            {
+                // Added to Rooms List
+
+                // instantiating list item in scroll view as a child of content
+                RoomListing listing = Instantiate(_roomListing, _content);
+                
+                if(listing != null)
+                {
+                    // setting the values of list item after instantiating it
+                    listing.SetRoomInfo(info);
+                    _listings.Add(listing);
+                }
+            }
+        }
+    }
+}
