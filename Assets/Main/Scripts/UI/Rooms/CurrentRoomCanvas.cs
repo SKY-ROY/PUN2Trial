7bb96b8f5da4efa5741b5cf9e03cb5a2fcc412fa
SKY <akash.rm.roy@gmail.com>
2021-04-15 11:35:00 +0530

    RPC fucntion calls and client specific interactivity implemented

--- Assets/Main/Scripts/UI/Rooms/CurrentRoomCanvas.cs
+++ Assets/Main/Scripts/UI/Rooms/CurrentRoomCanvas.cs
@@ -8,6 +8,8 @@ public class CurrentRoomCanvas : MonoBehaviour
     private PlayerListingMenu _playerListingMenu;
     [SerializeField]
     private LeaveRoomMenu _leaveRoomMenu;
+    public LeaveRoomMenu LeaveRoomMenu { get { return _leaveRoomMenu; } }
+
 
     private RoomsCanvases _roomsCanvases;
 
--- Assets/Main/Scripts/UI/Rooms/PLayerListingMenu.cs
+++ Assets/Main/Scripts/UI/Rooms/PLayerListingMenu.cs
@@ -2,6 +2,7 @@
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,15 +14,21 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
     [SerializeField]
     private PlayerListing _playerListing;
 
+    [SerializeField]
+    private TMP_Text _readyUpText;
+
     private List<PlayerListing> _listings = new List<PlayerListing>();
     private RoomsCanvases _roomsCanvases;
+    private bool _ready = false;
 
     public Button startGameButton;
-    
+    public Button readyUpButton;
+
     private void Awake()
     {
         // GetCurrentRoomPlayers();
         startGameButton.onClick.AddListener(OnClick_StartGame);
+        readyUpButton.onClick.AddListener(OnClick_ReadyUp);
     }
     
     public void FirstInitialize(RoomsCanvases canvases)
@@ -34,6 +41,10 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
         base.OnEnable();
         
         GetCurrentRoomPlayers();
+
+        SetReadyUp(false);
+
+        SetClientInteractivity();
     }
 
     public override void OnDisable()
@@ -53,6 +64,21 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
         _content.DestroyChildren();
     }
     */
+
+    private void SetReadyUp(bool state)
+    {
+        _ready = state;
+
+        if(_ready)
+        {
+            _readyUpText.text = "Ready";
+        }
+        else
+        {
+            _readyUpText.text = "Not Ready";
+        }
+    }
+
 
     /// <summary>
     /// creating a list of all the players currently in room
@@ -123,11 +149,33 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
         }
     }
 
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        // everyone leaves when master client leaves the room
+        _roomsCanvases.CurrentRoomCanvas.LeaveRoomMenu.OnClick_LeaveRoom();
+    }
+
+    // On click listener for Start-game button
     public void OnClick_StartGame()
     {
-        // client who creates the room is Master client
+        // client who creates the room is Master client and this snippet is specific to master client only
         if(PhotonNetwork.IsMasterClient)
         {
+            // traverse the entire player list
+            for(int i=0; i<_listings.Count; i++)
+            {
+                // check if current player is not equal to local player(master client)
+                if(_listings[i].Player != PhotonNetwork.LocalPlayer)
+                {
+                    // check if current player is ready or not
+                    if (!_listings[i].Ready)
+                    {
+                        Debug.Log(_listings[i].Player.NickName.ToString() + " is not ready!");
+                        return;
+                    }
+                }
+            }
+
             // prevents the users from seeing and joining the room so the next scene can be loaded 
             PhotonNetwork.CurrentRoom.IsOpen = false;
             PhotonNetwork.CurrentRoom.IsVisible = false;
@@ -135,6 +183,59 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
             // loading scene through scene index
             PhotonNetwork.LoadLevel(1);
         }
+    }
 
+    // On Click listener for Ready-Up button 
+    public void OnClick_ReadyUp()
+    {
+        if(!PhotonNetwork.IsMasterClient)
+        {
+            // opposite of ready on clicking it
+            SetReadyUp(!_ready);
+
+            // RPC function call with LocalPlayer and _ready as parameters,
+            // this RPC method targets the MasterClient from all the remote clients
+            base.photonView.RPC("RPC_ChangeReadyState", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer, _ready);
+
+        }
+    }
+
+    [PunRPC]
+    private void RPC_ChangeReadyState(Player player, bool ready)
+    {
+        // locate the index for the room in the list through name which has been deleted
+        int index = _listings.FindIndex(x => x.Player == player);
+
+        // if we find the room item being removed in the local list then destroy its gameObject in the UI
+        if (index != -1)
+        {
+            _listings[index].Ready = ready;
+        }
+    }
+
+    // changing the UI based on if player client is master client or not
+    private void SetClientInteractivity()
+    {
+        if(PhotonNetwork.IsMasterClient)
+        {
+            // action specific to MasterClient only
+            
+            // disabling interactivity of ready up button for local player(Master Client)
+            readyUpButton.interactable = false;
+        }
+        else
+        {
+            // action specific to remote clients only
+
+            // disabling interactivity of start game button for local player(non Master Client)
+            startGameButton.interactable = false;
+        }
+        
+        // changing the Host player(Master Client) listing background color to distinguish
+        int index = _listings.FindIndex(x => x.Player.UserId == PhotonNetwork.MasterClient.UserId);
+        if (index != -1)
+        {
+            _listings[index].gameObject.GetComponent<RawImage>().color = Color.cyan;
+        }
     }
 }
