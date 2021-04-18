37d47128bc375993ea980b6f0ef74069560b51c5
SKY <akash.rm.roy@gmail.com>
2021-04-18 15:53:01 +0530

    Animation synchronization over the network

---
+++ Assets/Main/Scripts/SingleUse/SimpleObjectMover.cs
@@ -0,0 +1,43 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleObjectMover : MonoBehaviourPun
+{
+    [SerializeField]
+    private float _moveSpeed = 1f;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        
+    }
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // to move the player's gameobject (using photonView.IsMine)
+        if(base.photonView.IsMine)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Vertical");
+
+            transform.position += (new Vector3(x, y, 0f) * _moveSpeed);
+            
+            // toggling the boolean for animation
+            UpdateMovingBoolean((x != 0f || y != 0f));
+        }
+    }
+
+    private void UpdateMovingBoolean(bool moving)
+    {
+        _animator.SetBool("moving", moving);
+    }
+}
